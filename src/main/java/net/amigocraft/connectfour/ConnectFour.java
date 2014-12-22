e32f8a703ae1fba9c9e50e39b30934bc70b28944
mproncace <mproncace@gmail.com>
2014-12-22 14:37:24 -0500

    Add heavy commenting

--- src/main/java/net/amigocraft/connectfour/ConnectFour.java
+++ src/main/java/net/amigocraft/connectfour/ConnectFour.java
@@ -18,128 +18,136 @@ public class ConnectFour {
 	private static boolean turn = true;
 	private static Scanner sc = new Scanner(System.in);
 
-	public static void initLoop(){
+	public static void initLoop(){ // initialize the game look
 		for (int[] row : pieces)
-			Arrays.fill(row, 0);
-		printBoard();
-		while (calcWinner() == 0){
-			AnsiConsole.out.println((turn ? ANSI_RED + "Player 1," : ANSI_CYAN + "Player 2,") + ANSI_WHITE + " it is your turn.");
-			int col;
+			Arrays.fill(row, 0); // fill the piece array
+		printBoard(); // render the board
+		while (calcWinner() == 0){ // while no one has one yet
+			AnsiConsole.out.println((turn ? ANSI_RED + "Player 1," : ANSI_CYAN + "Player 2,") + ANSI_WHITE +
+					" it is your turn.");
+			// notify whichever player that it's their turn
+			int col; // init so we can catch any user-triggered exceptions
 			try {
 				col = sc.nextInt() - 1;
 			}
-			catch (InputMismatchException ex){
+			catch (InputMismatchException ex){ // they entered a non-integer
 				AnsiConsole.out.println("Invalid input!");
 				continue;
 			}
-			catch (NoSuchElementException ex){
+			catch (NoSuchElementException ex){ // the program is being terminated
 				break; // clean termination
 			}
-			if (col >= pieces.length){
+			if (col < 1 || col >= pieces.length){ // piece is out of range
 				AnsiConsole.out.println("Invalid column!");
-				continue;
+				continue; // player retains turn
 			}
-			boolean placed = false;
-			for (int i = 0; i < pieces[col].length; i++){
-				if (pieces[col][i] == 0){
-					pieces[col][i] = turn ? 1 : 2;
-					placed = true;
-					break;
+			boolean placed = false; // whether the piece has been placed yet
+			for (int i = 0; i < pieces[col].length; i++){ // if this loop finishes the column is full
+				if (pieces[col][i] == 0){ // if there is no piece present yet
+					pieces[col][i] = turn ? 1 : 2; // place it for whichever player
+					placed = true; // piece has been placed
+					break; // no need to continue with the loop, that would screw things up a lot
 				}
 			}
-			if (!placed){
-				AnsiConsole.out.println("Column is full!");
-				continue;
+			if (!placed){ // could not place piece
+				AnsiConsole.out.println("Column is full!"); // notify
+				continue; // player retains turn
 			}
-			printBoard();
-			int winner = calcWinner();
-			if (winner > 0){
-				AnsiConsole.out.println((winner == 1 ? ANSI_RED : ANSI_CYAN) + "Player " + winner + ANSI_WHITE + " wins!");
+			printBoard(); // render the board now that a piece has been placed
+			int winner = calcWinner(); // call the method to calculate a potential winner
+			if (winner > 0){ // someone has won
+				AnsiConsole.out.println((winner == 1 ? ANSI_RED : ANSI_CYAN) + "Player " + winner + ANSI_WHITE +
+						" wins!"); // notify
 				try {
-					Object obj = new Object();
-					synchronized(obj){
-						obj.wait(3000L);
+					Object obj = new Object(); // exclusively for hanging the thread
+					synchronized (obj){ // required to obtain object lock
+						obj.wait(3000L); // wait 3 seconds before exiting
 					}
 				}
-				catch (InterruptedException ex){} // oh, well
-				return;
+				catch (InterruptedException ex){
+				} // oh, well
+				return; // no need to continue
 			}
-			turn = !turn;
+			turn = !turn; // pass the turn to the other player
 		}
 	}
 
 	public static void printBoard(){
+		// print the column numbers
 		AnsiConsole.out.print(ANSI_WHITE + "( ");
 		for (int i = 0; i < pieces.length; i++)
 			AnsiConsole.out.print((i + 1) + " ");
 		AnsiConsole.out.print(")");
-		AnsiConsole.out.println();
-		for (int j = pieces[0].length - 1; j >= 0; j--){
-			AnsiConsole.out.print(ANSI_YELLOW + "| ");
-			for (int i = 0; i < pieces.length; i++){
-				int res = pieces[i][j];
-				AnsiConsole.out.print((res == 0 ? ANSI_YELLOW + "O" : (res == 1 ? ANSI_RED : ANSI_CYAN) + res + ANSI_WHITE) + " ");
+		AnsiConsole.out.println(); // spacer
+		for (int j = pieces[0].length - 1; j >= 0; j--){ // iterate rows
+			AnsiConsole.out.print(ANSI_YELLOW + "| "); // edge of board
+			for (int i = 0; i < pieces.length; i++){ // iterate colums
+				int res = pieces[i][j]; // get piece at index
+				AnsiConsole.out.print((res == 0 ?
+						ANSI_YELLOW + "O" :
+						(res == 1 ? ANSI_RED : ANSI_CYAN)
+								+ res + ANSI_WHITE) + " ");
 			}
-			AnsiConsole.out.print(ANSI_YELLOW + "|");
-			AnsiConsole.out.println();
+			AnsiConsole.out.print(ANSI_YELLOW + "|"); // edge of board
+			AnsiConsole.out.println(); // spacer
 		}
 	}
 
 	public static int calcWinner(){
-		//AnsiConsole.out.println("All: " + checkCols() + ", " + checkRows() + ", " + checkDiags());
-		return Math.max(checkCols(), Math.max(checkRows(), checkDiags()));
+		//AnsiConsole.out.println("All: " + checkCols() + ", " + checkRows() + ", " + checkDiags()); // debug
+		return Math.max(checkCols(), Math.max(checkRows(), checkDiags())); // check all possible arrangements
 	}
 
-	private static int checkCols(){
-		for (int i = 0; i < pieces.length; i++){
-			int streak = 0;
-			int last = 0;
-			for (int j = 0; j < pieces[i].length; j++){
-				if (last != 0){
-					if (pieces[i][j] == last){
-						if (streak == 3){
+	private static int checkCols(){ // checks for vertical win
+		for (int i = 0; i < pieces.length; i++){ // iterate columns
+			int streak = 0; // current consecutive piece streak
+			int last = 0; // last piece color
+			for (int j = 0; j < pieces[i].length; j++){ // iterate vertically through rows
+				if (last != 0){ // there was a piece
+					if (pieces[i][j] == last){ // streak continues
+						if (streak == 3){ // this is the fourth in a row
 							//System.out.println("Col: " + i + ", " + j);
-							return pieces[i][j];
+							return pieces[i][j]; // return the winner
 						}
 						else
-							streak += 1;
+							streak += 1; // streak continues
 					}
 					else {
-						streak = 1;
+						streak = 1; // new streak
 						last = pieces[i][j];
 					}
 				}
 				else {
 					last = pieces[i][j];
-					streak = last == 0 ? 0 : 1;
+					streak = last == 0 ? 0 : 1; // reset streak
 				}
 			}
 		}
-		return 0;
+		return 0; // no winner
 	}
 
-	private static int checkRows(){
-		for (int j = 0; j < pieces[0].length; j++){
+	private static int checkRows(){ // checks rows for horizontal victory
+		for (int j = 0; j < pieces[0].length; j++){ // iterate rows
 			int last = 0;
 			int streak = 0;
-			for (int i = 0; i < pieces.length; i++){
+			for (int i = 0; i < pieces.length; i++){ // iterate columns
 				if (last != 0){
 					if (pieces[i][j] == last){
-						if (streak == 3){
+						if (streak == 3){ // this is the fourth in a row
 							//System.out.println("Row: " + i + ", " + j);
-							return pieces[i][j];
+							return pieces[i][j]; // return the winner
 						}
 						else
-							streak += 1;
+							streak += 1; // streak continues
 					}
 					else {
-						streak = 1;
+						streak = 1; // new streak
 						last = pieces[i][j];
 					}
 				}
 				else {
 					last = pieces[i][j];
-					streak = last == 0 ? 0 : 1;
+					streak = last == 0 ? 0 : 1; // reset streak
 				}
 			}
 		}
@@ -150,40 +158,39 @@ public class ConnectFour {
 		return Math.max(checkUpDiags(), checkDownDiags());
 	}
 
-	private static int checkUpDiags(){
-		for (int i = 0; i < pieces.length - 3; i++){
+	private static int checkUpDiags(){ // checks for diagonal victory going right and up
+		for (int i = 0; i < pieces.length - 3; i++){ // iterate rows
 			genLoop:
-			for (int j = 0; j < pieces[i].length - 3; j++){
+			for (int j = 0; j < pieces[i].length - 3; j++){ // iterate columns
 				int prec = pieces[i][j];
 				if (prec == 0)
-					continue genLoop;
-				for (int off = 1; off <= 3; off++){
+					continue;
+				for (int off = 1; off <= 3; off++){ // check for diagonal 4
 					if (pieces[i + off][j + off] != prec)
-						continue genLoop;
+						continue genLoop; // no victory
 				}
 				//AnsiConsole.out.println("Up diag: " + i + ", " + j);
-				return prec;
+				return prec; // return winner
 			}
 		}
-		return 0;
+		return 0; // no winner
 	}
 
-	private static int checkDownDiags(){
-		for (int i = 0; i < pieces.length - 3; i++){
+	private static int checkDownDiags(){ // checks for diagonal victory going right and down
+		for (int i = 0; i < pieces.length - 3; i++){ // iterate columns
 			genLoop:
-			for (int j = pieces[i].length - 1; j >= 3; j--){
+			for (int j = pieces[i].length - 1; j >= 3; j--){ // iterate rows
 				int prec = pieces[i][j];
 				if (prec == 0)
-					continue genLoop;
-				for (int off = 1; off <= 3; off++){
+					continue;
+				for (int off = 1; off <= 3; off++){ // check for diagonal 4
 					if (pieces[i + off][j - off] != prec)
-						continue genLoop;
+						continue genLoop; // no victory
 				}
 				//AnsiConsole.out.println("Down diag: " + i + ", " + j);
-				return prec;
+				return prec; // return winner
 			}
 		}
-		return 0;
+		return 0; // no winner
 	}
-
 }
