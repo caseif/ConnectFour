9c2730bfa862ab2911337fa8cf97fe3b9ff91a77
mproncace <mproncace@gmail.com>
2015-01-04 22:13:16 -0500

    Track main class

---
+++ src/main/java/net/amigocraft/connectfour/Main.java
@@ -0,0 +1,19 @@
+package net.amigocraft.connectfour;
+
+/**
+ * The main class, which calls the class that actually orchestrates the game.
+ * This serves only to serve the assignment requirements, and is neither necessary nor practical.
+ * @author Maxim Roncace
+ * @version 1.0.2
+ */
+public class Main {
+
+	/**
+	 * The main method of the app.
+	 * @param args required parameters
+	 */
+	public static void main(String[] args){
+		ConnectFour.initLoop();
+	}
+
+}
